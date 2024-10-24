5b65bcf3378af570dc4af74bcf58abe25e5f6a2c
Armando <armandocandelasalvarado@gmail.com>
2024-10-24 12:27:59 -0600

    first commit

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'frontend_musica',
-  webDir: 'www'
-};
-
-export default config;
